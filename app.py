3d480e05e9ced369561243501420a668e6a834c6
Des Dulianto <desdulianto@gmail.com>
2026-01-26 19:44:30 +0700

    add function to get user input and and exit system if user choose to

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from auth import login
 from user_service import register_user
 
+def get_user_action():
+    import sys
+    while True:
+        action = input("login/register: ")
+        if action in ('login', 'register'):
+            return action
+        if action == 'quit':
+            sys.exit(0)
+
 def main():
     print("Welcome to User System")
 
-    action = input("login/register: ")
+    action = get_user_action()
 
     if action == "login":
         u = input("username: ")
