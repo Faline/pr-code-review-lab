27556315aa79e0a748f3b8842fe4d1181ccb8b98
Faline <falinechen13@gmail.com>
2026-01-30 10:51:45 +0700

    Update user_service.py

--- user_service.py
+++ user_service.py
@@ -1,8 +1,16 @@
 import json
+from utils import is_empty
 
 def register_user(username, password):
+    if is_empty(username) or is_empty(password):
+        raise ValueError("Username dan password tidak boleh kosong")
+
     with open("data/users.json") as f:
         users = json.load(f)
+
+    for user in users:
+        if user["username"] == username:
+            raise ValueError("Username sudah terdaftar")
 
     users.append({
         "username": username,
@@ -11,4 +19,3 @@ def register_user(username, password):
 
     with open("data/users.json", "w") as f:
         json.dump(users, f)
-
