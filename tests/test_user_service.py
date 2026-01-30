d901e05a6492f44dcb01ac06d41d561f783bba05
Faline <falinechen13@gmail.com>
2026-01-30 10:52:52 +0700

    Update test_user_service.py

--- tests/test_user_service.py
+++ tests/test_user_service.py
@@ -1,6 +1,10 @@
+import pytest
 from user_service import register_user
 
-def test_register():
-    register_user("test", "123")
+def test_register_user_success():
+    register_user("userbaru", "password123")
     assert True
 
+def test_register_user_empty_username():
+    with pytest.raises(ValueError):
+        register_user("", "password123")
